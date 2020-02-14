d81c3ce6c4c8d63e11b3f19eb73ace5c13f85d0f
Francisco Chaves <francisco.jschaves@gmail.com>
2020-02-14 10:39:50 -0300

    Utilize async/await
    A palavra async faz que a função automaticamente retorne uma Promise.

--- src/02-nodejs/index.js
+++ src/02-nodejs/index.js
@@ -49,37 +49,29 @@ function obterEndereco(idUsuario, callback) {
 
 const obterEnderecoAsync = util.promisify(obterEndereco);
 
-const usuarioPromise = obterUsuario();
+async function main() {
+  try {
+    console.time("medida-promise");
+    const usuario = await obterUsuario();
+    // const telefone = await obterTelefone(usuario.id);
+    // const endereco = await obterEnderecoAsync(usuario.id);
+    const resultado = await Promise.all([
+      obterTelefone(usuario.id),
+      obterEnderecoAsync(usuario.id)
+    ]);
+    const telefone = resultado[0];
+    const endereco = resultado[1];
 
-usuarioPromise
-  .then(function(usuario) {
-    return obterTelefone(usuario.id).then(function resolverTelefone(result) {
-      return {
-        usuario: {
-          nome: usuario.nome,
-          id: usuario.id
-        },
-        telefone: result
-      };
-    });
-  })
-  .then(function(resultado) {
-    const endereco = obterEnderecoAsync(resultado.usuario.id);
-    return endereco.then(function resolverEndereco(result) {
-      return {
-        usuario: resultado.usuario,
-        telefone: resultado.telefone,
-        endereco: result
-      };
-    });
-  })
-  .then(function(resultado) {
     console.log(`
-      Nome: ${resultado.usuario.nome},
-      Endereço: ${resultado.endereco.rua}, ${resultado.endereco.numero}
-      Telefone: (${resultado.telefone.ddd}) ${resultado.telefone.telefone}
+      Nome: ${usuario.nome},
+      Endereço: ${endereco.rua}, ${endereco.numero}
+      Telefone: (${telefone.ddd}) ${telefone.telefone}
     `);
-  })
-  .catch(function(error) {
+    console.timeEnd("medida-promise");
+
+    return usuario;
+  } catch (error) {
     console.error("DEU RUIM", error);
-  });
+  }
+}
+main();
