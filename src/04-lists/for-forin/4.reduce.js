46215f31889d0989336b76b0dac49b8e309a22ee
Francisco Chaves <francisco.jschaves@gmail.com>
2020-02-15 19:02:24 -0300

    Crie uma função reduce genérica

--- src/04-lists/for-forin/4.reduce.js
+++ src/04-lists/for-forin/4.reduce.js
@@ -1,5 +1,13 @@
 const { obterPessoa } = require("./service");
 
+Array.prototype.meuReduce = function(callback, valorInicial) {
+  let valorFinal = typeof valorInicial != undefined ? valorInicial : this[0];
+  for (let index = 0; index <= this.length; index++) {
+    valorFinal = callback(valorFinal, this[index], this);
+  }
+  return valorFinal;
+};
+
 async function main() {
   try {
     const { results } = await obterPessoa("a");
@@ -10,6 +18,18 @@ async function main() {
       return anterior + proximo;
     });
     console.log("Total:", total);
+
+    const minhaLista = [
+      ["Francisco", "Chaves"],
+      ["NodeBr", "Nerdzão"]
+    ];
+
+    const lista = minhaLista
+      .meuReduce((anterior, proximo) => {
+        return anterior.concat(proximo);
+      }, [])
+      .join(", ");
+    console.log("Lista:", lista);
   } catch (error) {
     console.error("ERROR", error);
   }
