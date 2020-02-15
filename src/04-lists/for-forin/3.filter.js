9064e6e597511850cfd9ffcb8a5d073792e21147
Francisco Chaves <francisco.jschaves@gmail.com>
2020-02-15 14:03:44 -0300

    Crie uma função filter genérica

--- src/04-lists/for-forin/3.filter.js
+++ src/04-lists/for-forin/3.filter.js
@@ -10,20 +10,37 @@ const { obterPessoa } = require("./service");
   console.log(nome, idade)
 */
 
+Array.prototype.meuFilter = function(callback) {
+  const lista = [];
+  for (const index in this) {
+    const item = this[index];
+    const resultado = callback(item, index, this);
+    if (!resultado) continue;
+    lista.push(item);
+  }
+  return lista;
+};
+
 async function main() {
   try {
     const { results } = await obterPessoa("a");
 
-    const familiaLars = results.filter(function(item) {
-      // por padrão precisa retorna um boolean
-      // para informar se deve manter ou remover da lista
-      // false -> remove da lista
-      // true ->  mantêm
-      // não encontrou = -1
-      // encontrou = posição do array
-      const result = item.name.toLowerCase().indexOf("lars") !== -1;
-      return result;
+    // const familiaLars = results.filter(function(item) {
+    //   // por padrão precisa retorna um boolean
+    //   // para informar se deve manter ou remover da lista
+    //   // false -> remove da lista
+    //   // true ->  mantêm
+    //   // não encontrou = -1
+    //   // encontrou = posição do array
+    //   const result = item.name.toLowerCase().indexOf("lars") !== -1;
+    //   return result;
+    // });
+
+    const familiaLars = results.meuFilter((item, index, lista) => {
+      console.log(`index: ${index}`, lista.length);
+      return item.name.toLowerCase().indexOf("lars") !== -1;
     });
+
     const names = familiaLars.map(pessoa => pessoa.name);
     console.log(names);
   } catch (error) {
